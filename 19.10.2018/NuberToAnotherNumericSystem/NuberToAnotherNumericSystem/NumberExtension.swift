1add1e6036e6bccecc0869d60f017b74d282ce1a
Artem Kislitsyn <arcilite.kd@gmail.com>
2018-10-19 07:12:45 +0300

    19.09.2018 number to another numeric system

---
+++ 19.10.2018/NuberToAnotherNumericSystem/NuberToAnotherNumericSystem/NumberExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NumberExtension.swift
+//  NuberToAnotherNumericSystem
+//
+//  Created by Artem Kislitsyn on 19.10.2018.
+//  Copyright © 2018 3. All rights reserved.
+//
+
+import Foundation
