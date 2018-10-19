d0629b6718f8144cc7002d73ae0d0d8da0956a7d
Artem Kislitsyn <arcilite.kd@gmail.com>
2018-10-19 07:13:24 +0300

    19.10.2018 number to another numeri system kata

--- 19.10.2018/NuberToAnotherNumericSystem/NuberToAnotherNumericSystem/NumberExtension.swift
+++ 19.10.2018/NuberToAnotherNumericSystem/NuberToAnotherNumericSystem/NumberExtension.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+extension Int
+{
+	
+	public func numberWithBase(base:Int) -> String {
+		let baseDigits:[String] =  [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" ];
+		var numberToConvert = self;
+		var binaryNumber:[Int] = [Int](repeating: 0, count: 100)
+		var i = 0, number = 0;
+		var string:String = ""
+		
+		while (numberToConvert != 0) {
+			binaryNumber[i] = numberToConvert % base;
+			i+=1
+			numberToConvert          = numberToConvert / base;
+		}
+		
+		for j in (0...i - 1).reversed(){
+			number = binaryNumber[j]
+			string.append(String(baseDigits[number]))
+		}
+		
+		return string
+		
+	}
+	
+}
+
+//(id)convertedNumberWithBase:(int)base
+//{
+//	if (base != 2 && base != 8 && base != 10 && base != 12 && base != 16) {
+//		NSLog(@"Bad base - base must be 2, 8, 10, 12 or 16 only");
+//		base = 10;
+//	}
+//
+//	const char baseDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+//	long int numberToConvert = [self longValue];
+//	int convertedNumber[64];
+//	int nextDigit = 0, index = 0;
+//	id resultOfConvertion = nil;
+//	NSMutableString *resultString = [@"" mutableCopy];
+//
+//
+//	do {
+//		convertedNumber[index] = numberToConvert % base;
+//		++index;
+//		numberToConvert = numberToConvert / base;
+//	}
+//		while (numberToConvert != 0);
+//
+//	for (--index; index >= 0; --index) {
+//		nextDigit = convertedNumber[index];
+//		[resultString appendString:[NSString stringWithFormat:@"%c", baseDigits[nextDigit]]];
+//	}
+//
+//	if (base == 2 || base == 8 || base == 10) {
+//		NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+//		NSNumber *myNumber   = [f numberFromString:resultString];
+//		resultOfConvertion   = myNumber;
+//	}
+//	else {
+//		resultOfConvertion = [resultString copy];
+//	}
+//
+//	return resultOfConvertion;
+//}
--- 19.10.2018/NuberToAnotherNumericSystem/NuberToAnotherNumericSystemTests/NuberToAnotherNumericSystemTests.swift
+++ 19.10.2018/NuberToAnotherNumericSystem/NuberToAnotherNumericSystemTests/NuberToAnotherNumericSystemTests.swift
@@ -11,24 +11,40 @@ import XCTest
 
 class NuberToAnotherNumericSystemTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func testBinaryToDecimal() {
+		//arrange
+		let num = 2
+		
+		//act
+		let result = num.numberWithBase(base:2)
+		
+		//assert
+		XCTAssertEqual(result, "10", "not equal")
+		
+	}
+	
+	func test16ToDecimal() {
+		//arrange
+		let num = 12
+		
+		//act
+		let result = num.numberWithBase(base:16)
+		
+		//assert
+		XCTAssertEqual(result, "C", "not equal")
+		
+	}
+	
+	func test8ToDecimal() {
+		//arrange
+		let num = 9
+		
+		//act
+		let result = num.numberWithBase(base:8)
+		
+		//assert
+		XCTAssertEqual(result, "11", "not equal")
+		
+	}
 
 }
