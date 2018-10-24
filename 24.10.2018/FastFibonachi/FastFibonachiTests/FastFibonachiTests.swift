8bd71ea6eda47a801c5a059890fa9058f8d5c0f5
Artem Kislitsyn <arcilite.kd@gmail.com>
2018-10-24 06:58:34 +0300

    24.10.2018 fast fibonachi

--- 24.10.2018/FastFibonachi/FastFibonachiTests/FastFibonachiTests.swift
+++ 24.10.2018/FastFibonachi/FastFibonachiTests/FastFibonachiTests.swift
@@ -11,24 +11,13 @@ import XCTest
 
 class FastFibonachiTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func testFibonachi() {
+		//arrange
+		let numberFib  = 9
+		//act
+		let result = numberFib.fastFibonachi64()
+		//assert
+		XCTAssertEqual(34,result, "not equal")
+	}
 
 }
--- 24.10.2018/FastFibonachi/FibonachiExtension.swift
+++ 24.10.2018/FastFibonachi/FibonachiExtension.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+extension Int
+{
+	public func fastFibonachi64() -> UInt64 {
+		
+		var beforeLast:UInt64 = 0, last:UInt64 = 1;
+		var index = self
+		while (index > 0) {
+			index-=1
+			last += beforeLast;
+			beforeLast = last - beforeLast;
+		}
+		
+		if (index == 0) {
+			return beforeLast;
+		}
+		
+		return last;
+	}
+}
