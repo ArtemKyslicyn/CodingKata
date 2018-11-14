438b9ad15568a3d4ef0e8bfd285b6ce70a1c4635
Artem Kislitsyn <arcilite.kd@gmail.com>
2018-11-14 07:21:41 +0300

    14.10.2018  Decorator

---
+++ 14.11.2018/Decorator/Decorator/Decorator.swift
@@ -0,0 +1,9 @@
+//
+//  Decorator.swift
+//  Decorator
+//
+//  Created by Artem Kislitsyn on 14.11.2018.
+//  Copyright © 2018 3. All rights reserved.
+//
+
+import Foundation
---
+++ 14.11.2018/Decorator/DecoratorTests/DecoratorTests.swift
@@ -0,0 +1,34 @@
+//
+//  DecoratorTests.swift
+//  DecoratorTests
+//
+//  Created by Artem Kislitsyn on 14.11.2018.
+//  Copyright © 2018 3. All rights reserved.
+//
+
+import XCTest
+@testable import Decorator
+
+class DecoratorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
