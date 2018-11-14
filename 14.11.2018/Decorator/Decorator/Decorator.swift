c2a31227d347aa99243a89d888cf2224580a865f
Artem Kislitsyn <arcilite.kd@gmail.com>
2018-11-14 07:22:09 +0300

    14.10.2018 Decorator

--- 14.11.2018/Decorator/Decorator/Decorator.swift
+++ 14.11.2018/Decorator/Decorator/Decorator.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+protocol Coffee {
+	func getCost() -> Double
+	func getIngredients() -> String
+}
+
+class SimpleCoffee: Coffee {
+	func getCost() -> Double {
+		return 1.0
+	}
+	
+	func getIngredients() -> String {
+		return "Coffee"
+	}
+}
+
+class CoffeeDecorator: Coffee {
+	private let decoratedCoffee: Coffee
+	fileprivate let ingredientSeparator: String = ", "
+	
+	required init(decoratedCoffee: Coffee) {
+		self.decoratedCoffee = decoratedCoffee
+	}
+	
+	func getCost() -> Double {
+		return decoratedCoffee.getCost()
+	}
+	
+	func getIngredients() -> String {
+		return decoratedCoffee.getIngredients()
+	}
+}
+
+final class Milk: CoffeeDecorator {
+	required init(decoratedCoffee: Coffee) {
+		super.init(decoratedCoffee: decoratedCoffee)
+	}
+	
+	override func getCost() -> Double {
+		return super.getCost() + 0.5
+	}
+	
+	override func getIngredients() -> String {
+		return super.getIngredients() + ingredientSeparator + "Milk"
+	}
+}
+
+final class WhipCoffee: CoffeeDecorator {
+	required init(decoratedCoffee: Coffee) {
+		super.init(decoratedCoffee: decoratedCoffee)
+	}
+	
+	override func getCost() -> Double {
+		return super.getCost() + 0.7
+	}
+	
+	override func getIngredients() -> String {
+		return super.getIngredients() + ingredientSeparator + "Whip"
+	}
+}
